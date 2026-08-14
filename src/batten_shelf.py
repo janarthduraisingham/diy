52dc09948de6acbdb96b7b483ea5b2ae11331cc6
Janarth Duraisingham <jduraisingham108@gmail.com>
2026-08-14 17:42:24 +0100

    refactor out parameters

--- src/batten_shelf.py
+++ src/batten_shelf.py
@@ -35,64 +35,68 @@ def cuboid(x, y, z, dx, dy, dz):
     ]
     return faces
 
+height = 2.25
 short = 0.0508
 fat = short*2
-length = 1
-depth = 0.75
+length = 0.8
+depth = 0.50
+
+top_shelf_height = 1.6
+middle_shelf_height = 1.3-fat
+bottom_shelf_height = 0.8-fat
 
 shelf_thickness = 0.02
 
 screw_radius = 0.005
+screw_clearance = 0.2
 
-back_batten_1 = cuboid(0, depth, 0.75, length, -short, fat)
-back_batten_2 = cuboid(0, depth, 1.5, length, -short, fat)
-back_batten_3 = cuboid(0, depth, 2.25, length, -short, fat)
+back_batten_1 = cuboid(0, depth, bottom_shelf_height, length, -short, fat)
+back_batten_2 = cuboid(0, depth, middle_shelf_height, length, -short, fat)
+back_batten_3 = cuboid(0, depth, top_shelf_height, length, -short, fat)
 
-left_batten_1 = cuboid(0, 0, 0.75, short, depth-short, fat)
-left_batten_2 = cuboid(0, 0, 1.5, short, depth-short, fat)
-left_batten_3 = cuboid(0, 0, 2.25, short, depth-short, fat)
+left_batten_1 = cuboid(0, 0, bottom_shelf_height, short, depth-short, fat)
+left_batten_2 = cuboid(0, 0, middle_shelf_height, short, depth-short, fat)
+left_batten_3 = cuboid(0, 0, top_shelf_height, short, depth-short, fat)
 
-right_batten_1 = cuboid(length-short, 0, 0.75, short, depth-short, fat)
-right_batten_2 = cuboid(length-short, 0, 1.5, short, depth-short, fat)
-right_batten_3 = cuboid(length-short, 0, 2.25, short, depth-short, fat)
+right_batten_1 = cuboid(length-short, 0, bottom_shelf_height, short, depth-short, fat)
+right_batten_2 = cuboid(length-short, 0, middle_shelf_height, short, depth-short, fat)
+right_batten_3 = cuboid(length-short, 0, top_shelf_height, short, depth-short, fat)
 
-shelf_1 = cuboid(0, 0, 0.75+fat, length, depth, shelf_thickness)
-shelf_2 = cuboid(0, 0, 1.5+fat, length, depth, shelf_thickness)
-shelf_3 = cuboid(0, 0, 2.25+fat, length, depth, shelf_thickness)
+shelf_1 = cuboid(0, 0, bottom_shelf_height+fat, length, depth, shelf_thickness)
+shelf_2 = cuboid(0, 0, middle_shelf_height+fat, length, depth, shelf_thickness)
+shelf_3 = cuboid(0, 0, top_shelf_height+fat, length, depth, shelf_thickness)
 
-screw_clearance = 0.25
-
-back_batten_1_screw_1 = cuboid(screw_clearance-screw_radius, depth-short, 0.75+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
-back_batten_1_screw_2 = cuboid(length-screw_clearance-screw_radius, depth-short, 0.75+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
+back_batten_1_screw_1 = cuboid(screw_clearance-screw_radius, depth-short, bottom_shelf_height+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
+back_batten_1_screw_2 = cuboid(length-screw_clearance-screw_radius, depth-short, bottom_shelf_height+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
 
 
-back_batten_2_screw_1 = cuboid(screw_clearance-screw_radius, depth-short, 1.5+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
-back_batten_2_screw_2 = cuboid(length-screw_clearance-screw_radius, depth-short, 1.5+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
+back_batten_2_screw_1 = cuboid(screw_clearance-screw_radius, depth-short, middle_shelf_height+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
+back_batten_2_screw_2 = cuboid(length-screw_clearance-screw_radius, depth-short, middle_shelf_height+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
 
-back_batten_3_screw_1 = cuboid(screw_clearance-screw_radius, depth-short, 2.25+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
-back_batten_3_screw_2 = cuboid(length-screw_clearance-screw_radius, depth-short, 1.5+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
+back_batten_3_screw_1 = cuboid(screw_clearance-screw_radius, depth-short, top_shelf_height+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
+back_batten_3_screw_2 = cuboid(length-screw_clearance-screw_radius, depth-short, top_shelf_height+0.5*fat-screw_radius, screw_radius, short+0.1, screw_radius)
 
-left_batten_1_screw_1 = cuboid(-0.1, screw_clearance-screw_radius, 0.75+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
-left_batten_1_screw_2 = cuboid(-0.1, depth-screw_clearance-screw_radius, 0.75+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+left_batten_1_screw_1 = cuboid(-0.1, screw_clearance-screw_radius, bottom_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+left_batten_1_screw_2 = cuboid(-0.1, depth-screw_clearance-screw_radius, bottom_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
 
-left_batten_2_screw_1 = cuboid(-0.1, screw_clearance-screw_radius, 1.5+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
-left_batten_2_screw_2 = cuboid(-0.1, depth-screw_clearance-screw_radius, 1.5+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+left_batten_2_screw_1 = cuboid(-0.1, screw_clearance-screw_radius, middle_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+left_batten_2_screw_2 = cuboid(-0.1, depth-screw_clearance-screw_radius, middle_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
 
-left_batten_3_screw_1 = cuboid(-0.1, screw_clearance-screw_radius, 2.25+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
-left_batten_3_screw_2 = cuboid(-0.1, depth-screw_clearance-screw_radius, 2.25+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+left_batten_3_screw_1 = cuboid(-0.1, screw_clearance-screw_radius, top_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+left_batten_3_screw_2 = cuboid(-0.1, depth-screw_clearance-screw_radius, top_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
 
-right_batten_1_screw_1 = cuboid(length-short, screw_clearance-screw_radius, 0.75+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
-right_batten_1_screw_2 = cuboid(length-short, depth-screw_clearance-screw_radius, 0.75+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+right_batten_1_screw_1 = cuboid(length-short, screw_clearance-screw_radius, bottom_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+right_batten_1_screw_2 = cuboid(length-short, depth-screw_clearance-screw_radius, bottom_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
 
-right_batten_2_screw_1 = cuboid(length-short, screw_clearance-screw_radius, 1.5+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
-right_batten_2_screw_2 = cuboid(length-short, depth-screw_clearance-screw_radius, 1.5+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+right_batten_2_screw_1 = cuboid(length-short, screw_clearance-screw_radius, middle_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+right_batten_2_screw_2 = cuboid(length-short, depth-screw_clearance-screw_radius, middle_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
 
-right_batten_3_screw_1 = cuboid(length-short, screw_clearance-screw_radius, 2.25+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
-right_batten_3_screw_2 = cuboid(length-short, depth-screw_clearance-screw_radius, 2.25+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+right_batten_3_screw_1 = cuboid(length-short, screw_clearance-screw_radius, top_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
+right_batten_3_screw_2 = cuboid(length-short, depth-screw_clearance-screw_radius, top_shelf_height+0.5*fat-screw_radius, 0.1+short, screw_radius, screw_radius)
 
-back_wall = cuboid(0, depth, 0, length, 0.01, 3)
-left_wall = cuboid(0-0.01, 0, 0, 0.01, depth, 3)
-right_wall = cuboid(length, 0, 0, 0.01, depth, 3)
+back_wall = cuboid(0, depth, 0, length, 0.01, height)
+left_wall = cuboid(0-0.01, 0, 0, 0.01, depth, height)
+right_wall = cuboid(length, 0, 0, 0.01, depth, height)
 
 ax.add_collection3d(Poly3DCollection(back_batten_1, facecolors='lightblue', edgecolors='black', alpha=0.7))
 ax.add_collection3d(Poly3DCollection(back_batten_2, facecolors='lightblue', edgecolors='black', alpha=0.7))
